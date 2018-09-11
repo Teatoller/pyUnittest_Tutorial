bd6d2d03729380bccbb82e67be25b5a131156e68
Teatoller <Steven.otieno1@gmail.com>
2018-09-11 08:31:34 +0300

    Initial Commit

---
+++ app/calculator.py
@@ -0,0 +1,11 @@
+# calculator.py
+class Calculator(object):
+    """docstring for Calculator"""
+
+    def add(self, x, y):
+        number_types = (int, float, complex)
+
+        if isinstance(x, number_types) and isinstance(y, number_types):
+            return x + y
+        else:
+            raise ValueError
